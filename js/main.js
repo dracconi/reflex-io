316216421cf5949321e629f624e5ab0d16c0f4f5
dracconi <kacmalina@gmail.com>
2016-06-18 13:56:01 +0200

    Added seeds and changed random mechanics

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+$.getScript("js/timer.js");
+$.getScript("js/functions.js");
 $.getJSON( "js/words.json").then(start);
 var typos = 0;
 var item;
@@ -10,10 +12,14 @@ function start(response){
   $("#times").html("");
 }
 $(document).ready(function(){
-$(".header").click(function(){
-  $(".header").slideUp();
-  regenerate();
-})
+  if(getParameterByName("seed")==undefined){
+    window.location.replace(window.location.href + "?seed=" + makeid());
+  }
+  Math.seed = getParameterByName("seed");
+  $("#start").click(function(){
+    $(".header").slideUp();
+    regenerate();
+  })
 $("body").keyup(function(e){
   if (String.fromCharCode(e.which).toLowerCase() == item.charAt(correct).toLowerCase()){
     $("#"+correct).addClass("active");
@@ -24,6 +30,7 @@ $("body").keyup(function(e){
       regenerate();
     }
   }else{
+    time += 1;
     typos++;
   }
 })
@@ -34,7 +41,7 @@ function regenerate(){
   resetTimer();
   startTimer();
   correct = 0;
-  item = words[Math.floor(Math.random()*words.length)];
+  item = words[Math.floor(Math.seededRandom(words.length,0))];
   text = "";
   for(i=0;i<item.length; i++){
     text += "<span id='"+i+"'>"+item.charAt(i)+"</span>"
